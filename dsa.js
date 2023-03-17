ee9739ad52656e6db97fc94ea31f460dabeaf13e
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-17 12:12:40 +0530

    updated

---
+++ dsa.js
@@ -0,0 +1,48 @@
+// Q 1
+
+//Q2 
+// var start = 5768;
+// var end =78656;
+
+// var start =5768;
+// var end =78656;
+// var sum =0;
+
+// for(var i= start+1; i<end ; i++){
+
+//     if(i % 2 == 0){
+//         sum +=i;
+//     }
+   
+// }
+// console.log(sum);
+
+// Q 3 
+// var number= 19;
+// var array =[3,5,79,45,0,-67,23,77,19,78,12];
+// var index =null;
+// var flag =false;
+// for(var i=0; i<array.length; i++){
+//     if( array[i] === number){
+//         index = i;
+//         flag =true;
+//     }
+// }
+
+// if(flag){
+//     console.log(index);
+// }
+// else{
+//     console.log("Not Found");
+// }
+
+// Q4 
+// var array =[34,45,567,78,89,34,67,87,34,78,89];
+// var obj ={};
+
+// for(var i=0; i<array.length; i++){
+//     obj[i] = array[i];
+// }
+// console.log(obj);
+
+
